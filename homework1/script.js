acf9b7aecc24b3dd74d5a881e6ea0ecc9c7fbc65
Serj_NOTE <mokretsovsa@gmail.com>
2021-12-30 22:10:19 +0300

    lesson1 added

---
+++ homework1/script.js
@@ -0,0 +1,32 @@
+
+const products = [
+  { id: 1, title: 'Notebook', price: 1000 },
+  { id: 2, title: 'Mouse', price: 100 },
+  { id: 3, title: 'Keyboard', price: 250 },
+  { id: 4, title: 'Gamepad', price: 150 },
+];
+
+const renderProduct = (id, title, price, imgUrl = 'loading.gif') => {
+  return `<div class="product">
+            <img width="30" src="${imgUrl}" alt="prod_photo" class="product__img">
+            <span class="product__title">${title}
+            </span>
+            <span class="product__price">${price} рублей
+            </span>
+            <button class="product__btn btn${id}">
+            Добавить
+            </button>
+          </div>`;
+};
+
+const renderCatalog = (list) => {
+
+  const productList = list.map(good => renderProduct(good.id, good.title, good.price)).join('');
+
+  document.querySelector('.cart__products').innerHTML = productList;
+
+};
+
+renderCatalog(products);
+
+
